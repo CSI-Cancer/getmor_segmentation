e602a2db934dacabc72ab0fe8b7c91e630f79643
rishvanth-kp <rishvanth@gmail.com>
2023-02-12 12:33:42 -0800

    added functions for feature extraction

--- src/feature_extraction.cpp
+++ src/feature_extraction.cpp
@@ -25,6 +25,9 @@
 #include "itkImageFileReader.h"
 #include "itkNumericSeriesFileNames.h"
 
+#include "itkLabelStatisticsImageFilter.h"
+#include "itkLabelImageToShapeLabelMapFilter.h"
+
 /******************************************************************************/
 /* Type definitions                                                           */
 /******************************************************************************/
@@ -45,6 +48,16 @@ using LargeIntegerImageType = itk::Image<LargeIntegerPixelType, Dimension>;
 using NameGeneratorType = itk::NumericSeriesFileNames;
 using ReaderType = itk::ImageFileReader<IntegerImageType>;
 using LargeIntegerReaderType = itk::ImageFileReader<LargeIntegerImageType>;
+
+using LabelStatsType = itk::LabelStatisticsImageFilter<IntegerImageType,
+                                                       LargeIntegerImageType>;
+using ShapeLabelObjectType = itk::ShapeLabelObject<LargeIntegerPixelType, 
+                                                   Dimension>;
+using ShapeLabelMapType = itk::LabelMap<ShapeLabelObjectType>;
+using LabelShapeType = 
+  itk::LabelImageToShapeLabelMapFilter<LargeIntegerImageType, 
+                                       ShapeLabelMapType>;
+
 
 /******************************************************************************/
 /* Readers                                                                    */
@@ -81,6 +94,88 @@ read_img(LargeIntegerReaderType::Pointer &reader,
     cerr << "ITK EXCEPTION:"  << endl
          << e << endl;
   }
+}
+
+
+/******************************************************************************/
+/* Feature extraction                                                         */
+/******************************************************************************/
+// Cell shape features: depends only on the mask
+// For each cell, add the the cell id, cell_x, cell_y, number of pixels,
+// elongation, and roundness.
+static void
+shape_features(const LargeIntegerImageType::Pointer &label_mask,
+               LabelShapeType::Pointer &shape_stats,
+               vector<vector<float>> &feature_vector) {
+
+  shape_stats->SetInput(label_mask);
+  shape_stats->Update();
+
+  ShapeLabelMapType::Pointer shape_label_map = shape_stats->GetOutput();
+
+  size_t n_objects = shape_label_map->GetNumberOfLabelObjects();
+  feature_vector.resize(n_objects + 1);
+
+  for (size_t i = 0; i < n_objects; ++i) {
+    ShapeLabelObjectType::Pointer label_object =
+      shape_label_map->GetNthLabelObject(i);
+
+    feature_vector[i+1].push_back(i+1);
+    feature_vector[i+1].push_back(label_object->GetCentroid()[0]);
+    feature_vector[i+1].push_back(label_object->GetCentroid()[1]);
+    feature_vector[i+1].push_back(label_object->GetNumberOfPixels());
+    // feature_vector[i+1].push_back(label_object->GetNumberOfPixelsOnBorder());
+    feature_vector[i+1].push_back(label_object->GetElongation());
+    feature_vector[i+1].push_back(label_object->GetRoundness());
+    // feature_vector[i+1].push_back(label_object->GetFlatness());
+  }
+}
+
+
+// Cell intensity features: computed for each channel based on the mask
+// For each cell and for each channel, add the the mean intensity,
+// standard deviation of intensity, minimum intensity, and maximum
+// intensity.
+static void
+intensity_features(const IntegerImageType::Pointer &in,
+                   const LargeIntegerImageType::Pointer &label_mask,
+                   LabelStatsType::Pointer &label_stats,
+                   vector<vector<float>> &feature_vector) {
+
+  label_stats->SetLabelInput(label_mask);
+  label_stats->SetInput(in);
+  label_stats->Update();
+
+  // cout << feature_vector.size() << endl;
+  // cout << label_stats->GetNumberOfLabels() << endl;
+  if (label_stats->GetNumberOfLabels() > 1)
+    assert(feature_vector.size() == label_stats->GetNumberOfLabels());
+
+  for (auto it = label_stats->GetValidLabelValues().begin();
+       it != label_stats->GetValidLabelValues().end(); ++it) {
+
+    feature_vector[*it].push_back(label_stats->GetMean(*it));
+    feature_vector[*it].push_back(label_stats->GetSigma(*it));
+    feature_vector[*it].push_back(label_stats->GetMinimum(*it));
+    feature_vector[*it].push_back(label_stats->GetMaximum(*it));
+  }
+
+}
+
+
+// format for writing to file
+static string
+format_feature_vector(const size_t frame_id,
+                      const vector<vector<float>> feature_vec) {
+  std::ostringstream oss;
+  for (auto it = feature_vec.begin() + 1; it != feature_vec.end(); ++it) {
+    oss << frame_id << "\t";
+    for (auto jt = (*it).begin(); jt != (*it).end(); ++jt) {
+      oss << *jt << "\t";
+    }
+    oss << endl;
+  }
+  return oss.str();
 }
 
 
@@ -128,8 +223,8 @@ main (int argc, char* argv[]) {
     csv_to_uint(channel_start_str, channel_start);
     const size_t n_channels = channel_start.size();
 
-    const string label_format = "watershed%04d";
-    const string frame_format = "Tile%06d";
+    const string label_format = "watershed%04d.tif";
+    const string frame_format = "Tile%06d.tif";
 
     /**************************************************************************/
     /* Initialize filters                                                     */
@@ -140,6 +235,10 @@ main (int argc, char* argv[]) {
     ReaderType::Pointer reader = ReaderType::New();
     LargeIntegerReaderType::Pointer reader_large_int = 
       LargeIntegerReaderType::New(); 
+
+    // feature extraction
+    LabelStatsType::Pointer feature_intensity_stats = LabelStatsType::New();
+    LabelShapeType::Pointer feature_shape_stats = LabelShapeType::New();
  
     // file handlers
     ofstream features_file(label_dir + sample_name + "_feature_vec.txt");
@@ -177,13 +276,21 @@ main (int argc, char* argv[]) {
       read_img(reader_large_int, frame_dir + label_files[i], in_label);
 
       // generate shape features
+      // feature extraction
+      vector<vector<float>> feature_vector;
+      shape_features(in_label, feature_shape_stats, feature_vector);
   
       // generate intensity features for each channel
       for (size_t j = 0; j < n_channels; ++j) {
         IntegerImageType::Pointer in_frame;
-        read_img(reader, label_dir + in_frame_files[i][j], in_frame);
+        read_img(reader, label_dir + in_frame_files[j][i], in_frame);
+        intensity_features(in_frame, in_label,
+                           feature_intensity_stats, feature_vector);
+      
       }
 
+      // write feature vector to file
+      features_file << format_feature_vector(start_offset + i, feature_vector);
     }
   
     /**************************************************************************/
