b95f167c40054e2d64ef86492fe4b79724ad0b14
rishvanth-kp <rishvanth@gmail.com>
2023-02-16 21:31:48 -0800

    added argumnets

--- src/cell_image.cpp
+++ src/cell_image.cpp
@@ -21,23 +21,53 @@
 #include <string>
 #include <vector>
 
+#include "itkImage.h"
+#include "itkRGBPixel.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkNumericSeriesFileNames.h"
+#include "itkImageRegionIteratorWithIndex.h"
+
+
 using std::cout;
 using std::cerr;
 using std::endl;
 using std::string;
 using std::vector;
 
+constexpr size_t Dimension = 2;
+
+using IntegerPixelType = uint8_t;
+using IntegerImageType = itk::Image<IntegerPixelType, Dimension>;
+using RGBPixelType = itk::RGBPixel<IntegerPixelType>;
+using RGBImageType = itk::Image<RGBPixelType, Dimension>;
+
 
 int
 main (int argc, char* argv[]) {
   try {
-    if (argc != 9) {
+    if (argc != 10) {
       cerr << argv[0]
-           << " <frame_dir> <out_dir> <feature_vector>" << endl
+           << " <frame_dir> <out_dir> <cell_list_file>" << endl
            << "\t<ch_use_vector> <red_start> <green_start>"
            << " <blue_start> <white_start> <out_frame_size>" << endl;
       return EXIT_FAILURE;
     }
+
+    const string frame_dir = argv[1];
+    const string out_dir = argv[2];
+    const string cell_list_file = argv[3];
+
+    const string ch_to_use = argv[4];
+    const size_t red_start = std::stoi(argv[5]);
+    const size_t green_start = std::stoi(argv[6]);
+    const size_t blue_start = std::stoi(argv[7]);
+    const size_t white_start = std::stoi(argv[8]);
+    const size_t frame_len = std::stoi(argv[9]); 
+
+    const string in_format = "Tile%06d.tif";
+
+
   }
   catch (const std::exception &e) {
     cerr << "ERROR: " << endl;
