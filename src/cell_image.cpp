7cbfc522d8ea9afb726b274932e7aadc8f265908
rishvanth-kp <rishvanth@gmail.com>
2023-02-17 20:27:47 -0800

    added code to create output image

--- src/cell_image.cpp
+++ src/cell_image.cpp
@@ -29,6 +29,9 @@
 #include "itkImageRegionIteratorWithIndex.h"
 
 
+/******************************************************************************/
+/* Type definitions                                                           */
+/******************************************************************************/
 using std::cout;
 using std::cerr;
 using std::endl;
@@ -42,10 +45,77 @@ using IntegerImageType = itk::Image<IntegerPixelType, Dimension>;
 using RGBPixelType = itk::RGBPixel<IntegerPixelType>;
 using RGBImageType = itk::Image<RGBPixelType, Dimension>;
 
+using NameGeneratorType = itk::NumericSeriesFileNames;
 
+using ReaderType = itk::ImageFileReader<IntegerImageType>;
+using WriterType = itk::ImageFileWriter<IntegerImageType>;
+using RGBWriterType = itk::ImageFileWriter<RGBImageType>;
+
+/******************************************************************************/
+/* Reader and writer                                                          */
+/******************************************************************************/
+void
+read_frame(ReaderType::Pointer &reader,
+           const string &file,
+           IntegerImageType::Pointer &in) {
+
+  reader->SetFileName(file);
+  try {
+    reader->Update();
+    in = reader->GetOutput();
+  }
+  catch (const itk::ExceptionObject &e) {
+    cerr << "ITK EXCEPTION:" << endl
+         << e << endl;
+  }
+}
+
+
+void
+write_frame(WriterType::Pointer &writer,
+            const string &file,
+            const IntegerImageType::Pointer &out) {
+
+  writer->SetFileName(file + ".tif");
+  writer->SetInput(out);
+  try {
+    writer->Update();
+  }
+  catch (const itk::ExceptionObject &e) {
+    cerr << "ITK EXCEPTION:" << endl
+         << e << endl;
+  }
+}
+
+/******************************************************************************/
+/* Misc.                                                                      */
+/******************************************************************************/
+// conver a csv string to vector of ints
+static void
+csv_to_uint(const string &in,
+            vector<size_t> &tokens) {
+
+  tokens.clear();
+  size_t pos = 0;
+  const char delim = ',';
+  size_t next_pos = in.find(delim);
+  while (next_pos != string::npos) {
+    tokens.push_back(std::stoi(in.substr(pos, next_pos - pos)));
+    pos = ++next_pos;
+    next_pos = in.find(delim, pos);
+  }
+  tokens.push_back(std::stoi(in.substr(pos)));
+}
+
+/******************************************************************************/
+/* Main                                                                       */
+/******************************************************************************/
 int
 main (int argc, char* argv[]) {
   try {
+    /**************************************************************************/
+    /* Parse arguments                                                        */
+    /**************************************************************************/
     if (argc != 10) {
       cerr << argv[0]
            << " <frame_dir> <out_dir> <cell_list_file>" << endl
@@ -58,7 +128,15 @@ main (int argc, char* argv[]) {
     const string out_dir = argv[2];
     const string cell_list_file = argv[3];
 
-    const string ch_to_use = argv[4];
+    const string ch_to_use_str = argv[4];
+    vector<size_t> ch_to_use;
+    csv_to_uint(ch_to_use_str, ch_to_use);
+    if (ch_to_use.size() != 4) {
+      cerr << "ERROR: ch_use_vector must of length 4" << endl;
+      return EXIT_FAILURE;
+    }
+    
+
     const size_t red_start = std::stoi(argv[5]);
     const size_t green_start = std::stoi(argv[6]);
     const size_t blue_start = std::stoi(argv[7]);
@@ -66,6 +144,87 @@ main (int argc, char* argv[]) {
     const size_t frame_len = std::stoi(argv[9]); 
 
     const string in_format = "Tile%06d.tif";
+
+    /**************************************************************************/
+    /* Initialization                                                         */
+    /**************************************************************************/
+    ReaderType::Pointer reader = ReaderType::New();
+    WriterType::Pointer writer = WriterType::New();
+    RGBWriterType::Pointer rgb_writer = RGBWriterType::New();
+    
+    NameGeneratorType::Pointer name_gen = NameGeneratorType::New();
+
+    // calculate number of channels
+    size_t n_channels = 0;
+    for (auto it = ch_to_use.begin(); it != ch_to_use.end(); ++it) {
+      if (*it != 0) ++n_channels;
+    } 
+    cout << "number of ch: " << n_channels << endl;
+
+    // Allocate output tile image
+    IntegerImageType::Pointer tile_img = IntegerImageType::New();
+    
+    IntegerImageType::RegionType::SizeType tile_size;
+    tile_size[0] = frame_len * n_channels;
+    tile_size[1] = frame_len; 
+     
+    IntegerImageType::RegionType::IndexType tile_start;
+    tile_start[0] = 0;
+    tile_start[1] = 0;
+
+    IntegerImageType::RegionType tile_region;
+    tile_region.SetSize(tile_size);
+    tile_region.SetIndex(tile_start);
+
+    IntegerImageType::PointType tile_origin;
+    tile_origin[0] = 0;
+    tile_origin[1] = 0;
+
+    IntegerImageType::SpacingType tile_spacing;
+    tile_spacing[0] = 1;
+    tile_spacing[1] = 1;
+
+    tile_img->SetOrigin(tile_origin);
+    tile_img->SetSpacing(tile_spacing);
+    tile_img->SetRegions(tile_region);
+    tile_img->Allocate();
+
+    // Allocate output RGB image
+    RGBImageType::Pointer rgb_img = RGBImageType::New();
+    
+    RGBImageType::RegionType::SizeType rgb_size;
+    rgb_size[0] = frame_len;
+    rgb_size[1] = frame_len;
+    
+    RGBImageType::RegionType::IndexType rgb_start;
+    rgb_start[0] = 0;
+    rgb_start[1] = 0;
+
+    RGBImageType::RegionType rgb_region;
+    rgb_region.SetSize(rgb_size);
+    rgb_region.SetIndex(rgb_start);
+
+    RGBImageType::PointType rgb_origin;
+    rgb_origin[0] = 0;
+    rgb_origin[1] = 0;
+
+    RGBImageType::SpacingType rgb_spacing;
+    rgb_spacing[0] = 1;
+    rgb_spacing[1] = 1;
+
+    rgb_img->SetOrigin(rgb_origin);
+    rgb_img->SetSpacing(rgb_spacing);
+    rgb_img->SetRegions(rgb_region);
+    rgb_img->Allocate();
+      
+
+ 
+ 
+
+    /**************************************************************************/
+    /* Process frames                                                         */
+    /**************************************************************************/
+
 
 
   }
